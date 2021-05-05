98fd8b550c09bd2c038dc1c1c03f79ca871837bc
MintSoftnix <darunporn.san@gmail.com>
2021-05-05 15:12:18 +0700

    Setup frontend and Connect mongodb

---
+++ backend/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// var client *mongo.Client
+type Authentication struct{
+	Username string
+	password string
+}
+func main() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	db := client.Database("GolangLogin")
+	c := db.Collection("auth")
+
+	fmt.Println(c)
+
+}
+
